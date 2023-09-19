3c54c625c2bc26c4f754da899174dcf3f6d9e72b
Jacob Kossler <jacob.kossler@gmail.com>
2023-09-19 16:21:20 -0400

    Created Maven project and imported interfaces

---
+++ mydb-impl/src/main/java/uga/cs4370/mydb/impl/App.java
@@ -0,0 +1,13 @@
+package uga.cs4370.mydb.impl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
