931a1c7bc177a0da8410f183895c5ea7445c163c
Kossler <jacob.kossler@gmail.com>
2023-09-20 19:32:48 -0400

    String Predicate

---
+++ mydb-impl/src/main/java/uga/cs4370/mydb/impl/StringNotEqual.java
@@ -0,0 +1,27 @@
+package uga.cs4370.mydb.impl;
+
+import java.util.List;
+import uga.cs4370.mydb.*;
+
+public class StringNotEqual implements Predicate {
+    private int index;
+    private String value;
+
+    public StringNotEqual(int index, String value) {
+        this.index = index;
+        this.value = value;
+    }
+
+    /**
+     * Checks a row for a condition and returns true
+     * if the row passes the predicate.
+     */
+    @Override
+    public boolean check(List<Cell> row) {
+        if (!(row.get(index).getAsString().equals(value))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
